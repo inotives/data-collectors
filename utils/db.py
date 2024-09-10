304b5b38bca854fc40f9dd479be2c81d20360bda
Toni <inotives@gmail.com>
2024-09-10 16:46:04 +0800

    add insert_df-to-postgres

--- utils/db.py
+++ utils/db.py
@@ -1,6 +1,7 @@
 from prefect import task, flow
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+from sqlalchemy.exc import SQLAlchemyError
 
 # custom
 from configs.settings import POSTGRES_DB_URL
@@ -28,13 +29,16 @@ def create_table(table_class):
 def insert_data2db(table_class, data, conflicts): 
     conn = SqlAlc(POSTGRES_DB_URL)
     
-    status = conn.update_col_data(table_class, data, conflicts)
+    status = conn.upsert_data(table_class, data, conflicts)
     print(status)
 
     conn.close()
 
 @task
 def upsert_data2db(table_class, data, conflicts, col2update):
+    """
+    Upsert dataframe to db
+    """
     conn = SqlAlc(POSTGRES_DB_URL)
 
     status = conn.update_col_data(table_class, data, conflicts, col2update)
@@ -44,6 +48,9 @@ def upsert_data2db(table_class, data, conflicts, col2update):
 
 @task
 def export_data2csv(table_class, output_file='output', filters=None, date_field=None, date_range=None):
+    """
+    Exporting data from DB to csv
+    """
     conn = SqlAlc(POSTGRES_DB_URL)
 
     status = conn.export_to_csv(
@@ -56,6 +63,41 @@ def export_data2csv(table_class, output_file='output', filters=None, date_field=
 
     return status
 
+def insert_dataframe_to_postgres(df: pd.DataFrame, table_name: str):
+    """
+    Inserts DataFrame data into a PostgreSQL table.
+    
+    Parameters:
+        df (pd.DataFrame): The DataFrame to insert into the PostgreSQL table.
+        table_name (str): The name of the table to insert data into.
+        db_params (dict): Database connection parameters.
+    """
+    # Create a SQLAlchemy engine for PostgreSQL
+    engine = create_engine(POSTGRES_DB_URL)
+    
+    # Define SQL command to create the table if it does not exist
+    create_table_sql = f"""
+    CREATE TABLE IF NOT EXISTS {table_name} (
+    {', '.join([f'{col} VARCHAR(255)' for col in df.columns])}
+    );
+    """
+
+    try:
+        with engine.connect() as connection:
+            # Execute the SQL command to create the table
+            connection.execute(text(create_table_sql))
+            
+            # Insert DataFrame into PostgreSQL table
+            df.to_sql(table_name, engine, if_exists='append', index=False)
+        
+        print(f"Data inserted into table '{table_name}' successfully.")
+
+    except SQLAlchemyError as e:
+        print(f"Error: {e}")
+    
+
+    
+    
      
 ''' -- FLOWS ------------------------------------------------------------- '''
 @flow
